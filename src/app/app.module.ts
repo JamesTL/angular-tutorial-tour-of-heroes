0d0b3e2a4c514b4626dd875f9f8e378caae02b00
jameslove <hello@jameslove.co.uk>
2017-07-30 15:31:13 +0100

    tutorial page  3 - Multiple Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HeroDetailComponent} from './hero-detail-component';
+
 
 import {AppComponent}  from './app.component';
 
@@ -8,7 +10,10 @@ import {AppComponent}  from './app.component';
     imports: [
         BrowserModule,
         FormsModule],
-    declarations: [AppComponent],
+    //declaration array contains a list of application components,pipes and directives that belong to the module
+    declarations: [
+        AppComponent,
+        HeroDetailComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
